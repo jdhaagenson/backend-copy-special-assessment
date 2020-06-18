1ebfe0759b276ae8000ff8b857a1acfddc5eb8c2
Jordan Haagenson <jdhaagenson@gmail.com>
2020-06-18 08:57:25 -0700

    finished with help of Joseph

--- copyspecial.py
+++ copyspecial.py
@@ -15,6 +15,10 @@ import sys
 import shutil
 import subprocess
 import argparse
+import pprint
+import glob
+
+pp = pprint.pprint
 
 
 def get_special_paths(dirname):
@@ -28,9 +32,8 @@ def get_special_paths(dirname):
     for file in files:
         if re.search(r'__\w+__', file):
             special.append(os.path.join(abspath, file))
-            print(os.path.join(abspath, file))
+    # print(special)
     return special
-
 
 def copy_to(path_list, dest_dir):
     '''given a list of file paths, copies those files into the given directory'''
@@ -41,29 +44,26 @@ def copy_to(path_list, dest_dir):
         shutil.copy(path, abspath)
     return
 
-
 def zip_to(path_list, dest_zip):
     '''given a list of file paths, zip those files up into the given zip path'''
-    cmd = "zip -j " + dest_zip + " '" + "' '".join(path_list) + "'"
-    print("command to be executed: ")
-    print("`zip -j <zipfile> <each of the file paths>")
-    completed_process = subprocess.run(cmd)
-    if completed_process.check_returncode() != 0:
-        sys.stderr.write(completed_process.check_output())
-    print(completed_process.check_returncode())
-    return completed_process.check_output()
-
+    cmd = ['zip', '-j', dest_zip]
+    files = " ".join(path_list)
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise 
 
 def main(args):
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
+    parser.add_argument('fromdir', default=".", help='source directory')
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    parser.add_argument('fromdir', help='source directory')
     ns = parser.parse_args(args)
-    print(dir(ns))
-    # args = parser.parse_args()
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
@@ -74,9 +74,13 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
-    if ns.args == 
-
-    
-
+    pathlist = get_special_paths(ns.fromdir)
+    if ns.todir:
+        return copy_to(pathlist, ns.todir)
+    elif ns.tozip:
+        return zip_to(pathlist, ns.tozip)
+    else:
+        return pp(pathlist)
+        
 if __name__ == "__main__":
     main(sys.argv[1:])
